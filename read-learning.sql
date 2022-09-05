92762c2fb8e46ea745382f3d8a74565dc3dbbab7
Alessio Tugnoli <77077281+yougnoli@users.noreply.github.com>
2022-09-05 15:34:19 +0200

    Create read-learning.sql

---
+++ read-learning.sql
@@ -0,0 +1,16 @@
+-- read Learning.json with sql server
+
+select 
+	json_value(b.value, '$.source') as source
+	,convert(date, json_value(b.value, '$.purchase_date')) as purchase_date
+	,convert(date, json_value(b.value, '$.start_date')) as [start_date]
+	,convert(date, json_value(b.value, '$.finish_date')) as finish_date
+	,json_value(b.value, '$.title') as title
+	,json_value(b.value, '$.notes') as notes
+	,json_query(b.value, '$.tags') as tags
+	,convert(float, json_value(b.value, '$.price')) as price
+	,convert(bit, json_value(b.value, '$.completed')) as completed
+from 
+	openrowset(bulk 'C:\Temp\Learning.json', single_clob) as a 
+	cross apply
+	openjson(BulkColumn) as b
