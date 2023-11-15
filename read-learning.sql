8e5f781dc07ac52e770f7c61ef0487457edd0600
Alessio Tugnoli <77077281+yougnoli@users.noreply.github.com>
2023-11-15 15:49:43 +0100

    Update read-learning.sql

--- read-learning.sql
+++ read-learning.sql
@@ -2,15 +2,13 @@
 -- Download Learning.json, put it in a Temp folder on C disk (path has to be the same as is in line 16)
 
 select 
-	json_value(b.value, '$.source') as source
-	,convert(date, json_value(b.value, '$.purchase_date')) as purchase_date
-	,convert(date, json_value(b.value, '$.start_date')) as [start_date]
-	,convert(date, json_value(b.value, '$.finish_date')) as finish_date
-	,json_value(b.value, '$.title') as title
-	,json_value(b.value, '$.notes') as notes
-	,json_query(b.value, '$.tags') as tags
-	,convert(float, json_value(b.value, '$.price')) as price
-	,convert(bit, json_value(b.value, '$.completed')) as completed
+	json_value(b.value, '$.source') as [source]
+	,convert(date, json_value(b.value, '$.purchase_date')) as [purchase_date]
+	,json_value(b.value, '$.title') as [title]
+	,json_query(b.value, '$.author') as [author]
+	,json_value(b.value, '$.notes') as [notes]
+	,json_query(b.value, '$.tags') as [tags]
+	,convert(float, json_value(b.value, '$.price')) as [price]
 	,json_value(b.value, '$.link') as link
 from 
 	openrowset(bulk 'C:\Temp\Learning.json', single_clob) as a 
